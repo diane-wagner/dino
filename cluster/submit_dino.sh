1bbc346eefccb91252d7cba72ab68e1cf3330a65
wagnerd <wagnerd@informatik.uni-freiburg.de>
2022-02-21 12:44:41 +0100

    Update --saveckp_freq 10 in submission script

--- cluster/submit_dino.sh
+++ cluster/submit_dino.sh
@@ -9,4 +9,4 @@ pip list
 
 source activate dino
 
-python -m torch.distributed.launch --nproc_per_node=8 main_dino.py --arch vit_small --data_path /data/datasets/ImageNet/imagenet-pytorch/train --output_dir /work/dlclarge2/wagnerd-metassl-experiments/dino/$EXPERIMENT_NAME --batch_size_per_gpu 40
+python -m torch.distributed.launch --nproc_per_node=8 main_dino.py --arch vit_small --data_path /data/datasets/ImageNet/imagenet-pytorch/train --output_dir /work/dlclarge2/wagnerd-metassl-experiments/dino/$EXPERIMENT_NAME --batch_size_per_gpu 40 --saveckp_freq 10
